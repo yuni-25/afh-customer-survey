6060314623a35396001279873bf5f499bef80a2b
yuni-25 <yuniarayurachmadini@gmail.com>
2026-06-29 11:44:36 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,2 @@
-const GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxJgZd5BQK5pmyOfY45pdFJCIr2kkU3XM_J5zXEcQVAdXPrd2tmtOaT-H4oRyyjzsns/exec";
+const GOOGLE_SCRIPT_URL =
+"https://script.google.com/macros/s/AKfycbxJgZd5BQK5pmyOfY45pdFJCIr2kkU3XM_J5zXEcQVAdXPrd2tmtOaT-H4oRyyjzsns/exec";
