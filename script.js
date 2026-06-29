f11e9871be716787345d7f593a674263c6a4e8a3
yuni-25 <yuniarayurachmadini@gmail.com>
2026-06-29 11:18:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,8 @@
-fetch("survey.json")
-  .then(res => res.json())
-  .then(json => {
-
-      const survey = new Survey.Model(json);
-
-      survey.onComplete.add(function(sender){
-
-          fetch(GOOGLE_SCRIPT_URL,{
-              method:"POST",
-              mode:"no-cors",
-              headers:{
-                  "Content-Type":"application/json"
-              },
-              body:JSON.stringify(sender.data)
-          });
-
-      });
-
-      survey.render("surveyContainer");
-
-  });
+fetch(GOOGLE_SCRIPT_URL,{
+    method:"POST",
+    mode:"no-cors",
+    headers:{
+        "Content-Type":"application/json"
+    },
+    body:JSON.stringify(sender.data)
+});
